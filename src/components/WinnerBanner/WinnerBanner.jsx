9ca3d782ac08212ee8cc2f3ccb356797c64464e6
Chris Cantoni <chris.cantoni@gmail.com>
2024-04-25 12:15:15 -0500

    Adjusted winner modal for improved UI.

--- src/components/WinnerBanner/WinnerBanner.jsx
+++ src/components/WinnerBanner/WinnerBanner.jsx
@@ -1,14 +1,17 @@
 import Button from '@mui/material/Button';
+import CloseIcon from '@mui/icons-material/Close';
 
 function WinnerBanner({closeModal, winner, dealCards}) {
 
     return (
         <div className="winner-container" onClick={(e) => {
             if (e.target.className === 'winner-container') closeModal();
-        }}>
+        }}> 
             <div className="winner">
+            <Button sx={{color: 'white'}} className="close-icon" onClick={closeModal}><CloseIcon /></Button>
         <h1>{winner}</h1>
         <Button variant="contained" onClick={dealCards}>Play Again?</Button>
+        
         </div>
         </div>
     )
